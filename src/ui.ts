61b6d3836977156421f9757fca2e63b6d38ef9a5
WDever <37620133+WDever@users.noreply.github.com>
2020-06-25 18:02:40 +0900

    feat: Improve ui (#2)

--- src/ui.ts
+++ src/ui.ts
@@ -2,5 +2,5 @@ import * as $ from "jquery";
 
 export function apply() {
   // 1. Apply narrow navbar with container-xl (max-width)
-  $(".pagehead.repohead").wrapAll(`<div class="container-xl"></div>`);
+  $(".pagehead.repohead").children().addClass("container-xl");
 }
