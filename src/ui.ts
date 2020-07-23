1135f4a8590b4512a1c5872dc5f642949b299ee5
KDH <ehdgua01@naver.com>
2020-07-23 14:13:11 +0900

    select element using html tag
    클래스명이 변경될 때마다 수정이 필요할 거 같은데 태그로 탐색하시는 게 어떠신가요?
    이 방식도 좋다고는 할 수 없지만 클래스명보다는 변경되는 빈도가 낮을 거 같아 추천드립니당 :)

--- src/ui.ts
+++ src/ui.ts
@@ -2,5 +2,5 @@ import * as $ from "jquery";
 
 export function apply() {
   // 1. Apply narrow navbar with container-xl (max-width)
-  $(".pagehead.repohead").children().addClass("container-xl");
+  $("main > div").children().addClass("container-xl");
 }
